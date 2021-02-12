d5299c24b04faa4f42306585945d21fb7ad2375a
youngjune <youngjune@youngjune-W65-67SH>
2021-02-12 12:25:57 +0900

    git 첫 테스트

---
+++ src/com/webOrabge/sample/cal.java
@@ -0,0 +1,7 @@
+package com.webOrabge.sample;
+
+public class cal {
+	public int multi(int x, int y) {
+		return x*y ;
+	}
+}
---
+++ src/com/webOrabge/sample/calTest.java
@@ -0,0 +1,15 @@
+package com.webOrabge.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calTest {
+
+	@Test
+	public void testMulti() {
+		cal cal = new cal();
+		assertEquals(6, cal.multi(2,3));
+	}
+
+}
